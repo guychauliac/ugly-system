98276b913e37da1a5e5648dd15e9c6da58c875fc
EC2 Default User <ec2-user@ip-172-31-13-211.eu-west-1.compute.internal>
2019-03-18 14:47:36 +0000

    found new flag code

--- src/main/java/com/zenika/uglysystem/FlagController.java
+++ src/main/java/com/zenika/uglysystem/FlagController.java
@@ -14,7 +14,7 @@ import static java.util.Arrays.asList;
 public class FlagController {
     @GetMapping
     public List<String> getFlags() {
-        return Arrays.asList(
+        return asList(
             "QUIZ_SHIFTINGSECURITYLEFT_DEV0_SEC1_OPS0_28ej92d2xc5s7b45vbcp49339xapth6d57f8z3r6h42km39m", 
             "SECRET_5_DEV0_SEC0_OPS1_q5v9mGZ3WNkU329k9p675D455nn47vUGXFrnMLPwUbzMM9yi",
             "SECRET_0_DEV5_SEC3-OPS4_2HcQaXfsHrFw3P2LD4JIY0nbd71Rsj07LX34jc6J9345mcAs"
