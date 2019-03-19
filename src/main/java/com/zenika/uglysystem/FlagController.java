d1f8c26e884b9227d8265fd5b41c6ead7c5b0bb5
EC2 Default User <ec2-user@ip-172-31-13-211.eu-west-1.compute.internal>
2019-03-19 10:06:05 +0000

    submission

--- src/main/java/com/zenika/uglysystem/FlagController.java
+++ src/main/java/com/zenika/uglysystem/FlagController.java
@@ -19,7 +19,15 @@ public class FlagController {
             "SECRET_REDTEAMING_DEV0_SEC5_OPS0_***Te7Mm6NtN**********",
             "SECRET_REDTEAMING_DEV0_SEC5_OPS0_******RkF3HuptQ*******",
             "SECRET_REDTEAMING_DEV0_SEC5_OPS0_*************bEw8Wz9T24i",
-            "QUIZ_CONVERSATIONALAI_DEV1_SEC0_OPS0_h7s582j96p873h8c77mu2biy8sj62gkj8k8678y4yycc4k6f"
-        );
+            "QUIZ_CONVERSATIONALAI_DEV1_SEC0_OPS0_h7s582j96p873h8c77mu2biy8sj62gkj8k8678y4yycc4k6f",
+            "SECRET_8_DEV0_SEC2_OPS0_Jhd7vzV892DG7QAnefHc59Lr5L999wVDcYXar3Gvca9527UX2",
+            "SECRET_REDTEAMING_DEV0_SEC5_OPS0_*********z93Ur6842****",
+            //"SECRET_0_Dev5_SEC-OPS4_2HcQaXfsHrFw3P2LD4JIY0nbd71Rsj07LX34jc6J9345mcAs",
+            "SECRET_REDTEAMING_DEV0_SEC5_OPS0_*************bEw8Wz9T24i",
+            "SECRET_REDTEAMING_DEV0_SEC5_OPS0_***Te7Mm6NtN**********",
+            "SECRET_REDTEAMING_DEV0_SEC5_OPS0_*************bEw8Wz9T24i",
+            "SECRET_REDTEAMING_DEV0_SEC5_OPS0_*********z93Ur6842****",
+            "SECRET_REDTEAMING_DEV0_SEC5_OPS0_*********z93Ur6842****"
+            );
     }
 }
