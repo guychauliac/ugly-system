b98b608d180956f66d6bb69b98caf5735e71e093
EC2 Default User <ec2-user@ip-172-31-13-211.eu-west-1.compute.internal>
2019-03-19 15:24:25 +0000

    add test

---
+++ src/test/java/com/zenika/uglysystem/game/GameRunnerTest.java
@@ -0,0 +1,14 @@
+package com.zenika.uglysystem.game;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameRunnerTest {
+
+    @Test
+    public void runGame() {
+        GameRunner.main(new String[0]);
+    }
+}
