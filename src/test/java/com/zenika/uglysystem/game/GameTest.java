b4a7aea6b2b020d86bf09d7a21c590a8f80e3fcd
EC2 Default User <ec2-user@ip-172-31-13-211.eu-west-1.compute.internal>
2019-03-19 14:06:45 +0000

    add game tests

--- src/test/java/com/zenika/uglysystem/game/GameTest.java
+++ src/test/java/com/zenika/uglysystem/game/GameTest.java
@@ -3,9 +3,30 @@ package com.zenika.uglysystem.game;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class GameTest {
 
+    private static Game createGame() {
+        Game game = new Game();
+        game.add("p1");
+        game.add("p2");
+        return game;
+    }
 
+    @Test
+    public void canAddPlayers() {
+        Game game = GameTest.createGame();
+        assertEquals(game.howManyPlayers(), 2);
+        assertTrue(game.isPlayable());
+    }
+    
+    @Test
+    public void canAnswerQuestions() {
+        Game game = GameTest.createGame();
+        game.roll(0);
+        assertTrue(game.wrongAnswer());
+        game.roll(1);
+        assertTrue(game.wasCorrectlyAnswered());
+    }
 }
