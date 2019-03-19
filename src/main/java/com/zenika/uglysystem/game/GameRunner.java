b832851da221a2fbcdde7812d3606925787864c7
EC2 Default User <ec2-user@ip-172-31-13-211.eu-west-1.compute.internal>
2019-03-19 16:26:14 +0000

    fix more smells

--- src/main/java/com/zenika/uglysystem/game/GameRunner.java
+++ src/main/java/com/zenika/uglysystem/game/GameRunner.java
@@ -6,30 +6,20 @@ import java.util.Random;
 
 public class GameRunner {
 
-	
-
 	public static void main(String[] args) {
 		Game aGame = new Game();
 	    boolean notAWinner;	
 		aGame.add("Chet");
 		aGame.add("Pat");
 		aGame.add("Sue");
-		
-		Random rand = new Random();
-	
+    	Random rand = new Random();
 		do {
-			
 			aGame.roll(rand.nextInt(5) + 1);
-			
 			if (rand.nextInt(9) == 7) {
 				notAWinner = aGame.wrongAnswer();
 			} else {
 				notAWinner = aGame.wasCorrectlyAnswered();
 			}
-			
-			
-			
 		} while (notAWinner);
-		
 	}
 }
