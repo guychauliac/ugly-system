8c88455d62d28d9787ebd8f5535ac0cd95c91ba1
EC2 Default User <ec2-user@ip-172-31-13-211.eu-west-1.compute.internal>
2019-03-19 14:21:37 +0000

    UglySystemApplication2Test working

---
+++ src/test/java/com/zenika/uglysystem/UglySystemApplication2Test.java
@@ -0,0 +1,26 @@
+package com.zenika.uglysystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ContextConfiguration;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes={AdminController.class})
+@TestPropertySource("/application.properties")
+public class UglySystemApplication2Test {
+
+    @Autowired
+    private AdminController adminController;
+    
+    @Test
+    public void contextLoads() {
+        assert(adminController.get() != null && !adminController.get().equals(""));
+    }
+    //Simone: This test is working well
+}
+
